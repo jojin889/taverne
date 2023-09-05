9cfd36d0c72856d072915195232646296de25d94
Jonathan Theron <jonathan.theron@gmx.fr>
2023-09-05 16:02:24 +0200

    changed node sass version

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import News from "../components/News";
+import News from "../components/news";
 import Hero from "../components/hero";
 import Footer from "../components/footer";
 import Mail from "../components/contact_mail";
